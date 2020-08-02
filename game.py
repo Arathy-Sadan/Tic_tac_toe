bbc0be2aea2777adf4f4824074f07030de510f12
Arathy Sadan <arathysadan@gmail.com>
2020-08-02 09:55:08 +0530

    screen,logo,tittle

---
+++ game.py
@@ -0,0 +1,13 @@
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# screen
+screen = pygame.display.set_mode((500, 500))
+
+# logo & tittle
+tittle = pygame.display.set_caption("TIC TAC TOE")
+logo = pygame.image.load('tictactoe.png')
+pygame.display.set_icon(logo)
+
