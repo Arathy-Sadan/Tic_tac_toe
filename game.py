3c1ea417b987d1642296d0630b540ffa2c8caa2f
Arathy Sadan <arathysadan@gmail.com>
2020-08-02 09:58:42 +0530

    game loop is added

--- game.py
+++ game.py
@@ -11,3 +11,9 @@ tittle = pygame.display.set_caption("TIC TAC TOE")
 logo = pygame.image.load('tictactoe.png')
 pygame.display.set_icon(logo)
 
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
